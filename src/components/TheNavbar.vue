e1060af363ae8b00309a1ae0d8587698cbcd1f5f
tadamiura <tadamiura@gmail.com>
2021-10-07 19:57:19 +0200

    add navbar element in a component and data properties

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="nav">
+        <div v-for="(viewPath,viewName) in views" :key="viewName">
+            <router-link :to='viewPath'>{{viewName}}</router-link>  |
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'TheNavbar',
+    data() {
+        return {
+            views: {
+            Home: '/',
+            About: '/about',
+            Popper: '/popper'
+            }
+        }
+    },
+}
+</script>
