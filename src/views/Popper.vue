e11ee57f3e656423165ca977142cf919c36a5fef
tadamiura <tadamiura@gmail.com>
2021-10-08 10:43:54 +0200

    import vue popper instead of vue3 popper and crete an example

--- src/views/Popper.vue
+++ src/views/Popper.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>Popper</div>
+  <popper
+    trigger="clickToOpen"
+    :options="{
+      placement: 'top',
+      modifiers: { offset: { offset: '0,10px' } }
+    }">
+    <div class="popper">
+      Popper Content
+    </div>
+
+    <button slot="reference">
+      Reference Element
+    </button>
+  </popper>
 </template>
 
 <script>
-export default {
+  import Popper from 'vue-popperjs';
+  import 'vue-popperjs/dist/vue-popper.css';
 
-}
+  export default {
+    components: {
+      'popper': Popper
+    },
+  }
 </script>
-
-<style>
-
-</style>
